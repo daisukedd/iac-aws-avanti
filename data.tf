295b9e40b30258deb58ed863b383fa47921a0d16
wlln <kevinwallen.contato@gmail.com>
2025-09-16 16:07:11 -0300

    feat: inicializa repositório IaC com Terraform e Ansible

---
+++ data.tf
@@ -0,0 +1,10 @@
+# Pega a última Amazon Linux 2 elegível para Free Tier
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+}
